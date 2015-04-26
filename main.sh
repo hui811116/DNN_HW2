eced898e98dc76a2d2750cafe30b0d3c60d3ce39
jasonmhwu <j001001j001001@gmail.com>
2015-04-26 12:25:24 +0800

    train.sh

--- main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./svm_struct/svm_empty_learn.app -c 0.01 /home/ahpan/DeepLearningHW2/DNN_HW2/Data/MLDS_HW1_RELEASE_v1/fbank/train.ark /home/ahpan/DeepLearningHW2/DNN_HW2/Data/MLDS_HW1_RELEASE_v1/label/train.lab 
