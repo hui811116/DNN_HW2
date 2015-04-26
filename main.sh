7f91f8397891903cec53c71146753ee8a6e968b6
jasonmhwu <j001001j001001@gmail.com>
2015-04-26 09:57:11 +0800

    main

---
+++ main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./svm_struct/svm_empty_learn.app -c 0.01 /home/ahpan/DeepLearningHW2/DNN_HW2/Data/MLDS_HW1_RELEASE_v1/fbank/train.ark /home/ahpan/DeepLearningHW2/DNN_HW2/Data/MLDS_HW1_RELEASE_v1/label/train.lab 
